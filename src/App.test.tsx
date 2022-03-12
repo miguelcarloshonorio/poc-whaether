e73d033836418b148e892f3457c9e1b3108c86b8
Miguel Carlos Honorio <miguel@prosulti.com.br>
2022-03-12 16:29:07 -0300

    feat(chart with fake data): fake data on charts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders index page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome here/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Wheater Charts', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Wheater Charts/i);
   expect(linkElement).toBeInTheDocument();
 });
